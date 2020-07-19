5e5a67d61ea6b967356930c2be01d3f3070a5c13
Gennaro-Farina <gennarojavierfarina@gmail.com>
2020-07-19 16:34:08 +0200

    [DEV] Managing start and end date + region filtering [alternatives are 'all' or <name of the region>]

--- CoV2019/Code/main.m
+++ CoV2019/Code/main.m
@@ -3,39 +3,44 @@ clear classes;
 import com.DataProvider.*;
 import com.Model.*;
 
+startDate = datenum('01-Gen-2020');
+endDate   = datenum('01-Mag-2020');
+region    = 'all';
+    
 try
     tic;
-    disp('reading the data...')
+    disp('reading the data from remote repository (need connection)...')
     dataTable = DataProvider_cls.getItalianData();
-    dataTable = DataProvider_cls.cleanData(dataTable);
+    dataTable = DataProvider_cls.filtering(dataTable, ...
+                'startDate',    startDate, ...
+                'endDate',      endDate, ...
+                'region',       region);
     disp('data have been correctly readed!')
+    
+    firstDateTimeRow = dataTable(datetime(dataTable.Date)==min(datetime(dataTable.Date)), :);
+    I0 = firstDateTimeRow.TotalCases;
 catch Exception
     disp("Errore while getting the data");
     disp(Exception.identifier);
 end
 
 try
-    model_obj = Model_cls(Model_cls.SEIR_MODEL_scl);
-    
-    y0 = [obj.N - obj.I0 - obj.E0 obj.E0 obj.I0 0 obj.I0];
-    [tout,y] = ode45(@(t,y) odeSEIR(t,y,obj.N,obj.beta,obj.gamma,obj.sigma), tspan, y0);
-    % save results
-    obj.t = tout;
-    obj.S = y(:,1);
-    obj.E = y(:,2);                    
-    obj.I = y(:,3);
-    obj.R = y(:,4);
-    obj.C = y(:,5);
-    % calculate derivatives
-    dy = zeros(length(tout),5);
-    for n = 1:length(tout)
-        dy(n,:) = odeSEIR(0,y(n,:),obj.N,obj.beta,obj.gamma,obj.sigma);
-    end
-    obj.dS = dy(:,1);
-    obj.dE = dy(:,2);                      
-    obj.dI = dy(:,3);                  
-    obj.dR = dy(:,4);
-    obj.dC = dy(:,5);    
+    %defining a test run
+    N = 1;
+    beta = 1.0;
+    gamma = 197/244; %An approximation given by current statistics (we don't consider future recovered cases as well as exposed or untraced infected)
+    sigma = 1/7;
+    tend = endDate - startDate; % i want just a value per day
+
+    model_obj = Model_cls(Model_cls.SEIR_MODEL_scl, ...
+                         'N',       N, ...
+                         'beta',    beta, ...
+                         'gamma',   gamma,...
+                         'sigma',   1/10);
+                     
+    model_obj.simulate_fcn('I0',    I0, ...
+                           'E0',    0, ...
+                           'tend',  tend);
 catch Exception
     disp(Exception.identifier);
     disp('Error when executing the model');
